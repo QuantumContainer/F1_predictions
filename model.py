ffd782a465413e8a7d3f2288a8c248e4571293bd
Bhavesh <bhaveshsahu09543@gmail.com>
2026-06-18 20:52:07 +0530

    Refactor train_model function for readability

--- model.py
+++ model.py
@@ -3,13 +3,7 @@ from sklearn.ensemble import RandomForestRegressor
 
 def train_model(X, y):
 
-    model = RandomForestRegressor(
-        n_estimators=1000,
-        max_depth=12,
-        min_samples_leaf=2,
-        random_state=42,
-        n_jobs=-1
-    )
+    model = RandomForestRegressor(n_estimators=1000,max_depth=12,min_samples_leaf=2,random_state=42,n_jobs=-1)
 
     model.fit(X, y)
 
